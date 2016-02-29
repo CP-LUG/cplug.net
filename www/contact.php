465c0a8becb9d813f4e6a1bbe6fdac249ff1fe6d
Alex Mayer <amayer5125@gmail.com>
2016-02-29 17:46:04 -0500

    Contact: Make Mailing List Sub/Un-Sub Work

--- www/contact.php
+++ www/contact.php
@@ -25,28 +25,27 @@
                     <li>Port: 6667 (not encrypted) or 6697 (encrypted)</li>
                     <li>Channel: #cplug</li>
                 </ul>
-
-                <p>If you're unfamiliar with IRC client's, are behind a proxy, or simply want to do it the easy way, you can use our web interface.</p>
+                <p>If you're unfamiliar with IRC client's, are behind a proxy, or simply want to do it the easy way, you can use our <a href="http://www.cplug.net/irc/" target="_blank">web interface</a>.</p>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <h3 class="blue">CPLUG Mailing List</h3>
                 <p>To subsribe to the list you select from the drop down menu what type of subscription you would like. <strong>The options are Regular, Digest and No Mail.</strong></p>
                 <p><strong>Regular</strong> subscription means that you may post to the list and and all postings to the list will be delivered to that address as they happen. </p>
                 <p><strong>Digest</strong> means that you will only get one email per day and that email will contain all the messages that were posted to the list that day. </p>
-                <p><strong>No Mail</strong> option is for folks that want to subscribe multiple addresses, however do not want a copy of every post to the list to go to each account. If you are unsure you probably want Regular.</p>
-                <form>
+                <p><strong>No Mail</strong> option is for folks that want to subscribe multiple addresses, however do not want a copy of every post to the list to go to each account.</p>
+                <p>If you are unsure you probably want Regular.</p>
+                <form action="http://cplug.net/mailing-list/mailer.php" method="post">
                     <fieldset class="form-group">
-                        <select class="form-control" name="subscription" id="exampleSelect1">
-                            <option default>Subscription Type</option>
-                            <option>Regular</option>
-                            <option>Digest</option>
-                            <option>No Mail</option>
+                        <select class="form-control" name="sub_type">
+                            <option value="regular" default>Regular</option>
+                            <option value="digest">Digest</option>
+                            <option value="nomail">No Mail</option>
                         </select>
                     </fieldset>
                     <fieldset class="form-group">
-                        <input type="email" class="form-control" name="email" placeholder="Enter email">
+                        <input type="email" class="form-control" name="sub_email" placeholder="Enter email">
                     </fieldset>
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
+                    <button type="submit" name="subscribe" class="btn btn-primary">Subscribe</button>
                     <button type="button" id="unsubscribe" class="btn btn-link">unsubscribe</button>
                 </form>
                 <hr>
@@ -54,9 +53,9 @@
                     <h4>To Unsubscribe:</h4>
                     <form >
                         <fieldset class="form-group">
-                            <input type="email" class="form-control" name="email" placeholder="Enter email">
+                            <input type="unsub_email" class="form-control" name="email" placeholder="Enter email">
                         </fieldset>
-                        <button type="submit" class="btn btn-default">Subscribe</button>
+                        <button type="submit" name="subscribe" class="btn btn-default">Subscribe</button>
                     </form>
                 </div>
             </div>
