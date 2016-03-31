25f325d6930e539835f3fdde2474adc818b3e78f
Alex Mayer <amayer5125@gmail.com>
2016-03-31 01:38:22 -0400

    Rework Copy On Directions Page

--- www/directions.php
+++ www/directions.php
@@ -17,18 +17,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6">
+                <h3 class="blue">Meeting Location</h3>
+                <p>
+                    ITT Tech, Harrisburg Campus<br>
+                    449 Eisenhower Boulevard, Suite 100<br>
+                    Harrisburg, PA 17111
+                </p>
+                <p>The Theory 9 Room is where we usually gather, but occasionally that room is unavailable for our use. If you can&rsquo;t find us, follow the signs for CPLUG.</p>
+
+                <h3 class="blue">Meeting Date &amp; Time</h3>
+                <p>2<sup>nd</sup> Tuesday of each month, 6:30 p.m.</p>
+
                 <h3 class="blue">Directions</h3>
-                <p>CPLUG holds monthly meetings with speakers speaking on a wide range of topics. Most of the speakers are group members, but occasionally special guest speakers come in and talk to the group. The meetings are geared towards Linux users of any experience level and everyone is welcome.</p>
-
-                <h3 class="blue">Meeting Date and Time</h3>
-                <p>Meetings are held on the second Tuesday of each Month at 6:30 PM.</p>
-                <img src="images/itt_tech-harrisburg_pa.jpg" class="img-responsive" />
-                <h3 class="blue">ITT Tech, Harrisburg Campus</h3>
-                <h4 class="blue">449 Eisenhower Boulevard, Suite 100</h4>
-                <p>Room: We usually meet in Theory 9, but sometimes we are in another room. Check out the IRC channel or twitter if you can’t find us.</p>
-
-                <h3 class="blue">Directions Summary</h3>
-                <p>ITT Tech is on the corner of Paxton St. and Eisenhower Blvd, just down from the Harrisburg Mall. CPLUG meets on the lower level, the simplest way to get there is to use the student entrance in the back.</p>
+                <p>ITT Tech is located on the corner of Paxton Street and Eisenhower Boulevard, under Belco, just down the road from the Harrisburg Mall. CPLUG meets on the lower level, and the simplest way to get there is to use the student entrance in the back.</p>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d97443.1579190608!2d-76.88493243987456!3d40.25133808869269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x89c8bf7a97838463%3A0x2ced247926299ced!2s449+Eisenhower+Boulevard%2C+Suite+100!3m2!1d40.251359199999996!2d-76.8148925!5e0!3m2!1sen!2sus!4v1455907141638" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
