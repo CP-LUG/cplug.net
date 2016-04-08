3cf98c27f95dd3488c8ae3098d1aa57e6bf68b0f
Nathan Hare <nhare@openmailbox.org>
2016-04-08 12:24:07 -0400

    Add clickable icons to point to Github and Meetup sites

--- www/inc/scripts_bottom.php
+++ www/inc/scripts_bottom.php
@@ -2,10 +2,14 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <div id="footer">
   <div class="container">
-    <div class="col-xs">
+      <a class="icon-link" href="http://www.meetup.com/cp-lug/" target="_blank">
+        <img  src="images/meetup.png" />
+      </a>
+      <a class="icon-link" href="https://github.com/cp-lug/cplug.net" target="_blank">
+        <img  src="images/github.png" />
+      </a>
       <u>Group Facilitator</u>: Nate Moss<br />
       <u>Site Maintainer</u>: Alex Mayer<br />
       <u>Contributers</u>: Ryan, Nathan Hare<br />
-    </div>
   </div>
 </div>
