ad29a4fc36aa1e33b9ea7743fa5b027c95d5ac03
Nathan Hare <nhare@openmailbox.org>
2016-04-08 13:56:49 -0400

    Remove namelist from footer

--- www/inc/scripts_bottom.php
+++ www/inc/scripts_bottom.php
@@ -1,15 +1,13 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <div id="footer">
-  <div class="container">
-      <a class="icon-link" href="http://www.meetup.com/cp-lug/" target="_blank">
+  <div class="container text-right" style="line-height:60px;">
+      Check us out on: &nbsp;&nbsp;&nbsp; 
+      <a href="http://www.meetup.com/cp-lug/" target="_blank">
         <img  src="images/meetup.png" />
       </a>
-      <a class="icon-link" href="https://github.com/cp-lug/cplug.net" target="_blank">
+      <a href="https://github.com/cp-lug/cplug.net" target="_blank">
         <img  src="images/github.png" />
       </a>
-      <u>Group Facilitator</u>: Nate Moss<br />
-      <u>Site Maintainer</u>: Alex Mayer<br />
-      <u>Contributers</u>: Ryan, Nathan Hare<br />
   </div>
 </div>
