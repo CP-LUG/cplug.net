643fd17c9de619e9a423c2a788c2f70dc25657cd
Nathan Hare <nhare@openmailbox.org>
2016-04-08 14:59:36 -0400

    Reduce height of footer by 25px

--- www/inc/scripts_bottom.php
+++ www/inc/scripts_bottom.php
@@ -1,7 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <div id="footer">
-  <div class="container text-right" style="line-height:60px;">
+  <div class="container text-right" style="line-height:40px;">
       Check us out on: &nbsp;&nbsp;&nbsp; 
       <a href="http://www.meetup.com/cp-lug/" target="_blank">
         <img  src="images/meetup.png" />
