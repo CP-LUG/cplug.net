3f8e9772a30ed8baf821d4fcc270a94563cca738
Nathan Hare <nhare@openmailbox.org>
2016-04-08 12:00:41 -0400

    Add sticky footer

--- www/inc/scripts_bottom.php
+++ www/inc/scripts_bottom.php
@@ -1,2 +1,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<div id="footer">
+  <div class="container">
+    <div class="col-xs">
+      <u>Group Facilitator</u>: Nate Moss<br />
+      <u>Site Maintainer</u>: Alex Mayer<br />
+      <u>Contributers</u>: Ryan, Nathan Hare<br />
+    </div>
+  </div>
+</div>
