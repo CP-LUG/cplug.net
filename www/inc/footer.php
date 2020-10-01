52b32753caf597cac78485a66fa06a0eb12cf640
Yogesh Pandey <incredibleyug@gmail.com>
2020-10-01 12:01:27 +0530

    improved footer.php
    Correct footer sentence to visit on another site of your community and also add an white space between images (links).

--- www/inc/footer.php
+++ www/inc/footer.php
@@ -1,9 +1,9 @@
 <footer>
   <div class="container text-right" style="line-height:40px;">
-      Check us out on: &nbsp;&nbsp;&nbsp; 
+      Also Visit At: &nbsp;&nbsp;&nbsp; 
       <a href="http://www.meetup.com/cp-lug/" target="_blank">
         <img  src="images/meetup.png" />
-      </a>
+      </a>&nbsp;
       <a href="https://github.com/cp-lug/cplug.net" target="_blank">
         <img  src="images/github.png" />
       </a>
